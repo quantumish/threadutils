02a8dff6054e39808861a8ed3a425c5dd4faf84e
David Freifeld <freifeld.david@gmail.com>
2021-01-21 17:48:28 -0800

    Added questionable shared_lock

--- threadutils.cpp
+++ threadutils.cpp
@@ -36,6 +36,7 @@ void semaphore::release()
 }
 
 class spinlock {
+protected:
     std::atomic_flag latch;
     std::thread::id owner;
 public:
@@ -55,6 +56,32 @@ void spinlock::unlock() {
     if (owner != std::this_thread::get_id()) return;
     latch.clear(std::memory_order_release);
 }
+
+class shared_lock : spinlock {
+    std::atomic<int> threads;
+    std::atomic_flag shared_latch;
+public:
+    shared_lock();
+    void s_lock();
+    void s_unlock();
+};
+
+shared_lock::shared_lock() :shared_latch(false), threads(0), spinlock() {}
+
+void shared_lock::s_lock()
+{
+    std::thread::id o{};
+    while(latch.test_and_set(std::memory_order_acquire) && owner != o);    
+    owner=o;
+    threads++;
+}
+
+void shared_lock::s_unlock()
+{
+    threads--;
+    if (threads==0) latch.clear();
+}
+
 
 template <class T>
 class List {
