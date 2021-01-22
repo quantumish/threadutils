c923fa8d7bc2607ef59f362cccf91b24ffb81bce
David Freifeld <freifeld.david@gmail.com>
2021-01-21 20:24:11 -0800

    Attempted to add member function support

--- monitor.cpp
+++ monitor.cpp
@@ -1,16 +1,17 @@
 #include <concepts>
 #include <functional>
+#include <eigen3/Eigen/Dense>
 #include "locks.hpp"
 
 template <class T> 
 class monitor {
     spinlock s;
     T val;
-public:
-    // HACK No clue how this works
-    // Based off of https://stackoverflow.com/questions/12647217/making-a-c-class-a-monitor-in-the-concurrent-sense
+public:    
     template <class R, class ...Args>
     R execute_function(std::function<R(T, Args...)> f, Args... args);
+    template <class R, class ...Args>
+    R execute_member(auto f, Args... args);
     monitor();
     monitor(T v);
     
@@ -21,24 +22,41 @@ monitor<T>::monitor() :val() {}
 template <class T> 
 monitor<T>::monitor(T v) :val(v) {}
 
+// C++ templates are my new religion. This is miraculous.
 template <class T>
 template <class R, class ...Args>
 R monitor<T>::execute_function(std::function<R(T, Args...)> f, Args... args)
 {
-    return f(val, args...);
+    s.lock();
+    R out = f(val, args...);
+    s.unlock();
+    return out;
 }
 
-void arbitary_function(char suffix, const char* w1, int w2, void* w3)
+template <class T>
+template <class R, class ...Args>
+R monitor<T>::execute_member(auto f, Args... args)
 {
-    std::cout << " " << w1 << " " << w2 << " " << w3 << " " << suffix << "\n";
+    s.lock();
+    R out = val.f(args...);
+    s.unlock();
+    return out;
+}
+
+int arbitrary_function(char suffix, const char* w1, int w2, void* w3)
+{
+    std::cout << w1 << " " << w2 << " " << w3 << " " << suffix << "\n";
+    return 2;
 }
 
 int main()
 {
-    monitor<char> a {'!'};
-    const char* c1 = "wtf this is magic";
-    int c2 = 3432;
-    void* c3 = (void*) 0x432f34;
-    std::function<void(char, const char*, int, void*)> f = arbitrary_function;
-    a.execute_function(f, c1, c2, c3);
+    monitor<Eigen::MatrixXf> a (Eigen::MatrixXf::Random(10,10));
+    // const char* c1 = "wtf this is magic";
+    // int c2 = 3432;
+    // void* c3 = (void*) 0x432f34;
+    // std::function<int(char, const char*, int, void*)> f = arbitrary_function;
+    Eigen::MatrixXf b = Eigen::MatrixXf::Random(10,10);
+    // std::function<const Eigen::Product(const Eigen::MatrixBase&))> f = Eigen::MatrixXf::operator*;
+    std::cout << a.execute_member(Eigen::MatrixXf::operator*, b)  << "\n";
 }
