265c16cf865aa17e1b5c6cf0202bc972d4b83058
David Freifeld <freifeld.david@gmail.com>
2021-01-23 11:26:18 -0800

    Monitor troubles

--- monitor.cpp
+++ monitor.cpp
@@ -5,23 +5,60 @@
 
 namespace thd {
 
-template <class T> 
+// Check that operators exist (should be true for all primitives).
+template <typename T>
+concept operatable = requires (T a, T b) {
+    a+b; a-b; a+b; a/b; a&b; a|b; a^b; a=b;
+};
+
+template <operatable T>
+class arithmetic_monitor {
+    spinlock s;
+public:
+    T val;
+    arithmetic_monitor();
+    arithmetic_monitor(T v);
+    T operator=(const T&);
+    T operator+(const T&);
+    T operator-(const T&);
+    T operator*(const T&);
+    T operator/(const T&);
+    T operator&(const T&);
+    T operator|(const T&);
+    T operator^(const T&);
+};
+
+template <operatable T>
+arithmetic_monitor<T>::arithmetic_monitor() :val() {}
+template <operatable T>
+arithmetic_monitor<T>::arithmetic_monitor(T v) :val(v) {}
+
+template <operatable T> T arithmetic_monitor<T>::operator=(const T& other) { s.lock(); val = other; s.unlock(); return val; }
+template <operatable T> T arithmetic_monitor<T>::operator+(const T& other) { s.lock(); T temp = val + other; s.unlock(); return temp; }
+template <operatable T> T arithmetic_monitor<T>::operator-(const T& other) { s.lock(); T temp = val - other; s.unlock(); return temp; }
+template <operatable T> T arithmetic_monitor<T>::operator*(const T& other) { s.lock(); T temp = val * other; s.unlock(); return temp; }
+template <operatable T> T arithmetic_monitor<T>::operator/(const T& other) { s.lock(); T temp = val / other; s.unlock(); return temp; }
+template <operatable T> T arithmetic_monitor<T>::operator&(const T& other) { s.lock(); T temp = val & other; s.unlock(); return temp; }
+template <operatable T> T arithmetic_monitor<T>::operator|(const T& other) { s.lock(); T temp = val | other; s.unlock(); return temp; }
+template <operatable T> T arithmetic_monitor<T>::operator^(const T& other) { s.lock(); T temp = val ^ other; s.unlock(); return temp; }
+
+template <class T>
 class monitor {
     spinlock s;
     T val;
-public:    
+public:
     template <class R, class ...Args>
     R execute_function(std::function<R(T, Args...)> f, Args... args);
     template <class R, class ...Args>
-    R execute_member(auto f, Args... args);
+    R execute_member(std::function<R(Args...)> f, Args... args);
     monitor();
     monitor(T v);
-    
+
 };
 
-template <class T> 
+template <class T>
 monitor<T>::monitor() :val() {}
-template <class T> 
+template <class T>
 monitor<T>::monitor(T v) :val(v) {}
 
 // C++ templates are my new religion. This is miraculous.
@@ -37,7 +74,7 @@ R monitor<T>::execute_function(std::function<R(T, Args...)> f, Args... args)
 
 template <class T>
 template <class R, class ...Args>
-R monitor<T>::execute_member(auto f, Args... args)
+R monitor<T>::execute_member(std::function<R(Args...)> f, Args... args)
 {
     s.lock();
     R out = val.f(args...);
@@ -55,12 +92,14 @@ int arbitrary_function(char suffix, const char* w1, int w2, void* w3)
 
 int main()
 {
-    thd::monitor<Eigen::MatrixXf> a (Eigen::MatrixXf::Random(10,10));
+    thd::arithmetic_monitor<int> a = 2;
     // const char* c1 = "wtf this is magic";
     // int c2 = 3432;
     // void* c3 = (void*) 0x432f34;
     // std::function<int(char, const char*, int, void*)> f = arbitrary_function;
-    Eigen::MatrixXf b = Eigen::MatrixXf::Random(10,10);
-    // std::function<const Eigen::Product(const Eigen::MatrixBase&))> f = Eigen::MatrixXf::operator*;
-    std::cout << a.execute_member(Eigen::MatrixXf::operator*, b)  << "\n";
+    // Eigen::MatrixXf b = Eigen::MatrixXf::Random(10,10);
+    int b = 3;
+    //a = b;
+    a+=1;
+    std::cout << a.val << "\n";
 }
