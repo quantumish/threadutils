586cd0e838e7c6721d5eafd2f6fc55db75356540
David Freifeld <freifeld.david@gmail.com>
2021-01-24 17:42:30 -0800

    Naive lockfree stack
    Doesn't account for ABA problem

--- stack.cpp
+++ stack.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <thread>
 #include <atomic>
 
 template <class T>
@@ -11,6 +12,9 @@ public:
     std::atomic<Node*> head;
     stack();
     void push(T value);
+    void pop();
+    T peek();
+    T& operator[](int index);
 };
 
 template <class T>
@@ -22,15 +26,54 @@ void stack<T>::push(T value)
     Node* update;
     Node* original = head.load(std::memory_order_acquire);
     Node* addition = new Node;
+    addition->val = value;
+    update = addition;
     do {
         addition->next = original;
-        update = addition;
     } while(!head.compare_exchange_weak(original, update));
 }
+
+template <class T>
+void stack<T>::pop()
+{
+    Node* update;
+    Node* original = head.load(std::memory_order_acquire);
+    do {
+        if (original->next == nullptr) return;
+        update = original->next;
+    } while(!head.compare_exchange_weak(original, update));
+    delete original;
+}
+
+template <class T>
+T stack<T>::peek()
+{
+    Node* top = head.load(std::memory_order_acquire);
+    return top->val;
+}
+
+// template <class T>
+// T& stack<T>::operator[](int index)
+// {
+//     Node* r = head.load(std::memory_order_acquire);
+//     for (int i = 0; i < index; i++) {
+//         std::cout << r << "\n" << r->next << "\n";
+//         r = r->next;
+//     }
+//     return r->val;
+// }
 
 int main()
 {
     stack<uint64_t> a;
-    a.push(3);
-    std::cout << (a.head.load(std::memory_order_acquire))->val << "\n";
+    std::thread t1(&stack<uint64_t>::push, &a, 1);
+    std::thread t2(&stack<uint64_t>::push, &a, 2);
+    std::thread t3(&stack<uint64_t>::push, &a, 3);
+    t1.join(); t2.join(); t3.join();
+    std::cout << a.head.load(std::memory_order_acquire)->val << "\n";
+    a.pop();
+    std::cout << a.head.load(std::memory_order_acquire)->val << "\n";
+    a.pop();
+    std::cout << a.head.load(std::memory_order_acquire)->val << "\n";
+    //std::cout << a[0] << " " << a[1] << " " << a[3] << "\n";
 }
