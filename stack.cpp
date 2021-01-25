1a1a06ea8446b85b129c690114b47b984e26f063
David Freifeld <freifeld.david@gmail.com>
2021-01-24 18:10:46 -0800

    Added tagged pointer for ABA problem

--- stack.cpp
+++ stack.cpp
@@ -8,60 +8,59 @@ class stack {
         T val;
         Node* next;
     };
+    struct tagptr {
+        uint64_t tag;
+        Node* ptr;
+    };
+    std::atomic<tagptr> head;
 public:
-    std::atomic<Node*> head;
     stack();
     void push(T value);
     void pop();
     T peek();
+    T top();
     T& operator[](int index);
 };
 
 template <class T>
-stack<T>::stack() :head{nullptr} {}
+stack<T>::stack() :head{{0, nullptr}} {}
 
 template <class T>
 void stack<T>::push(T value)
 {
-    Node* update;
-    Node* original = head.load(std::memory_order_acquire);
+    tagptr update;
+    tagptr original = head.load(std::memory_order_acquire);
     Node* addition = new Node;
     addition->val = value;
-    update = addition;
+    update.ptr = addition;
     do {
-        addition->next = original;
+        addition->next = original.ptr;
+        update.tag += 1;
     } while(!head.compare_exchange_weak(original, update));
 }
 
 template <class T>
 void stack<T>::pop()
 {
-    Node* update;
-    Node* original = head.load(std::memory_order_acquire);
+    tagptr update;
+    tagptr original = head.load(std::memory_order_acquire);
     do {
-        if (original->next == nullptr) return;
-        update = original->next;
+        if (original.ptr->next == nullptr) return;
+        update.ptr = original.ptr->next;
+        update.tag += 1;
     } while(!head.compare_exchange_weak(original, update));
-    delete original;
+    delete original.ptr;
 }
 
 template <class T>
 T stack<T>::peek()
 {
-    Node* top = head.load(std::memory_order_acquire);
-    return top->val;
+    tagptr top = head.load(std::memory_order_acquire);
+    return top.ptr->val;
 }
 
-// template <class T>
-// T& stack<T>::operator[](int index)
-// {
-//     Node* r = head.load(std::memory_order_acquire);
-//     for (int i = 0; i < index; i++) {
-//         std::cout << r << "\n" << r->next << "\n";
-//         r = r->next;
-//     }
-//     return r->val;
-// }
+template <class T>
+T stack<T>::top() {return peek();}
 
 int main()
 {
@@ -70,10 +69,9 @@ int main()
     std::thread t2(&stack<uint64_t>::push, &a, 2);
     std::thread t3(&stack<uint64_t>::push, &a, 3);
     t1.join(); t2.join(); t3.join();
-    std::cout << a.head.load(std::memory_order_acquire)->val << "\n";
+    std::cout << a.peek() << "\n";
     a.pop();
-    std::cout << a.head.load(std::memory_order_acquire)->val << "\n";
+    std::cout << a.peek() << "\n";
     a.pop();
-    std::cout << a.head.load(std::memory_order_acquire)->val << "\n";
-    //std::cout << a[0] << " " << a[1] << " " << a[3] << "\n";
+    std::cout << a.peek() << "\n";
 }
