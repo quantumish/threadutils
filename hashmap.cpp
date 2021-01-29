53163dcbe09f3c5634d7c9c093b36e9ccf08839e
David Freifeld <freifeld.david@gmail.com>
2021-01-29 15:54:30 -0800

    Rehashing explodes for strings

--- hashmap.cpp
+++ hashmap.cpp
@@ -5,7 +5,7 @@
 #include <cstdint>
 #include <string>
 
-#define TABLE_DEFAULT_SZ 15
+#define TABLE_DEFAULT_SZ 16
 
 template <class K, class V>
 class hashmap {
@@ -25,14 +25,14 @@ public:
 
 template <class K, class V>
 hashmap<K,V>::hashmap(float l)
-    :arr{new V[TABLE_DEFAULT_SZ]()}, /*karr{new K[TABLE_DEFAULT_SZ]}, */ size{TABLE_DEFAULT_SZ}, keys{0}, collisions{0}, load_max{l} {}
+    :arr{new V[TABLE_DEFAULT_SZ]()}, karr{new K[TABLE_DEFAULT_SZ]()}, size{TABLE_DEFAULT_SZ-1}, keys{0}, collisions{0}, load_max{l} {}
 
 template <class K, class V>
 void hashmap<K,V>::insert(K key, V val)
 {
     std::size_t index = std::hash<K>{}(key) & size;
-    std::cout << std::hash<K>{}(key) << " " << index << "\n";
-    if (load_factor() >= load_max) resize(size*2 + 1);
+    // std::cout << std::hash<K>{}(key) << " " << size << " " << index << "\n";
+    if (load_factor() >= load_max) resize(size*2 + 2);
     keys++;
     if (arr[index] != 0) collisions++;
     karr[index] = key;
@@ -53,15 +53,15 @@ void hashmap<K,V>::resize(size_t min)
     V* old = arr;
     K* kold = karr;
     arr = new V[min];
-    karr = new K[min];
+     karr = new K[min];
     for (int i = 0; i < size; i++) {
         size_t rehash = std::hash<K>{}(kold[i]) & min;
-        arr[i] = old[i];
+        arr[rehash] = old[i];
         karr[rehash] = kold[i];
     }
     delete old;
     delete kold;
-    size = min;
+    size = min - 1;
 }
 
 template <class K, class V>
@@ -85,13 +85,13 @@ std::string gen_random(const int len) {
 
 int main(int argc, char** argv)
 {
-    hashmap<std::string, int> h(0.7);
+    hashmap<int, int> h(0.7);
     #define MAXNUM 14000
     uint64_t BIGNUM = std::stoll(argv[1], NULL, 10);
     for (int i = 0; i < BIGNUM; i++) {
         // std::string s = gen_random(10);
         // std::cout << s << "\n";
-        h.insert(gen_random(10),rand()/(RAND_MAX/MAXNUM+1));
+        h.insert(rand()%BIGNUM,rand()/(RAND_MAX/MAXNUM+1));
     }
     std::cout << h.collisions << " " << (double)h.collisions / BIGNUM << "\n";
     // for (int i = 0; i < BIGNUM; i++) {
